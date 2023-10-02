44b6762d9ed50c560945c044a4654efd1e9d9e93
Karim518 <karim.boubaz5@gmail.com>
2023-10-02 17:09:10 +0200

    Test cryptage reussi

--- testcryptage.py
+++ testcryptage.py
@@ -2,11 +2,10 @@ import unittest
 from cryptage import *
 
 class Cryptage_Test(unittest.TestCase):
-    def setUp(self):
-        self.instance=cryptage()
 
-
-
+    def test_crypt(self):
+        self.assertEqual(crypt("Yo"), "Rp")
+        self.assertEqual(crypt("Salut ca va "), "Tbmvuadbawba")
 
 if __name__ == '__main__':
     unittest.main()
